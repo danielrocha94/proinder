95fd530cc77da47ccc01c8ca66e84753b2f14981
daniel.rocha1 <daniel.rocha@verycreatives.com>
2018-08-02 00:36:36 -0500

    Actualizando service name del connect String

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   hrPool: {
     user          : "admin",
     password      : "password",
-    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=proinder.cikpr4aojpml.us-west-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)))",
+    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=proinder.cikpr4aojpml.us-west-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=PROINDER)))",
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0
